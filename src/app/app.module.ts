c2adbdd47fe73bf529c23efa401c892384fe4a57
darklol <darklol_777@mail.ru>
2021-02-03 23:14:17 +0300

    comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { CommentContainerComponent } from './comment-container/comment-container.component';
 import { CommentComponent } from './comment/comment.component';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { CommentComponent } from './comment/comment.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 import { IComment } from './icomment';
 
@@ -9,17 +9,44 @@ import { IComment } from './icomment';
   providedIn: 'root'
 })
 export class CommentService {
-  private url = 'http://test-server/';
-  private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
+  private url = 'api/comments';
   constructor(private http: HttpClient) { }
 
   getComments(): Observable<IComment[]> {
-    return this.http.get<IComment[]>(this.url);
+    return this.http.get<IComment[]>(this.url).pipe(
+      map((comments) => {
+        this.getCommentsFromStorage(comments);
+
+        return  comments
+      })
+    );
   }
 
   addComment(comment): Observable<IComment> {
-    return this.http.post<IComment>(this.url, JSON.stringify(comment), this.httpOptions);
+    return this.http.post<IComment>(this.url, comment).pipe(
+      map((comment) => {
+        this.addCommentInStorage(comment);
+        
+        return comment
+      })
+    );
+  }
+
+  getCommentsFromStorage(comments) {
+    const localComments = JSON.parse(localStorage.getItem('comments'));
+    if (localComments !== null) {
+      comments.unshift(...localComments);
+    }
+  }
+
+  addCommentInStorage(comment: IComment) {
+    let localComments = JSON.parse(localStorage.getItem('comments'));
+    if (localComments === null) {
+      localComments = [];
+    }
+    localComments.push(comment);
+
+    const json = JSON.stringify(localComments);
+    localStorage.setItem('comments', json);
   }
 }
